6f7d4871c04724b4b1d8c3eec0e9abcd3fc3c5bc
victoriadenoblet <vict.noblet@hotmail.fr>
2020-12-17 18:16:58 +0100

    DetectivePlayer

--- src/fr.isep.JackPocket/DetectivePlayer.java
+++ src/fr.isep.JackPocket/DetectivePlayer.java
@@ -1,7 +1,15 @@
 package fr.isep.JackPocket;
 
+import fr.isep.JackPocket.Action.ActionToken;
+
+import java.util.List;
+
 public class DetectivePlayer {
-    private boolean isDetective = true;
+    private boolean isDetective;
+
+    public void launcherActionToken(List<ActionToken> actionTokenList){
+
+    }
 
 
 }
